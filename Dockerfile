ebd039a2658b322358d938813245fb55518f8cb7
isolo <ilya.i.solo@gmail.com>
2016-09-16 20:30:46 +0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM vromero/activemq-artemis
+EXPOSE 1098 1099
+COPY docker-entrypoint.sh /
---
+++ docker-entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+# Log to tty to enable docker logs container-name
+sed -i "s/logger.handlers=.*/logger.handlers=CONSOLE/g" ../etc/logging.properties
+
+# Update users and roles with if username and password is passed as argument
+if [[ "$ARTEMIS_USERNAME" && "$ARTEMIS_PASSWORD" ]]; then
+  sed -i "s/amq=artemis/amq=$ARTEMIS_USERNAME/g" ../etc/artemis-roles.properties
+  sed -i "s/artemis=simetraehcapa/$ARTEMIS_USERNAME=$ARTEMIS_PASSWORD/g" ../etc/artemis-users.properties
+fi
+
+# Update min memory if the argument is passed
+if [[ "$ARTEMIS_MIN_MEMORY" ]]; then
+  sed -i "s/-Xms512M/-Xms$ARTEMIS_MIN_MEMORY/g" ../etc/artemis.profile
+fi
+
+# Update max memory if the argument is passed
+if [[ "$ARTEMIS_MAX_MEMORY" ]]; then
+  sed -i "s/-Xmx1024M/-Xmx$ARTEMIS_MAX_MEMORY/g" ../etc/artemis.profile
+fi
+
+if [[ "$JMX_REMOTE" ]]; then
+  sed -i '/^JAVA_ARGS/a JAVA_ARGS="$JAVA_ARGS -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=1099 -Dcom.sun.management.jmxremote.rmi.port=1098 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.local.only=false -Djava.rmi.server.hostname=192.168.99.100"' ../etc/artemis.profile
+fi
+
+if [ "$1" = 'artemis-server' ]; then
+  set -- gosu artemis "./artemis" "run"
+fi
+
+exec "$@"
